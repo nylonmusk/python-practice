10be32e4241bdea5c39cec2e06a1074bace46c32
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-01-07 18:17:10 +0900

    feat : quiz4

--- python-practice/quiz3.py
+++ python-practice/quiz3.py
@@ -1,6 +1,7 @@
 # quiz3
 
-# 표준 체중을 구하는 프로그램을 작성하시오
+# 표준 체
+# 중을 구하는 프로그램을 작성하시오
 
 # * 표준 체중 : 각 개인의 키에 적당한 체중
 
@@ -9,8 +10,8 @@
 # 여자 : 키(m) x 키(m) x 21
 
 # 조건 1 : 표준 체중은 별도의 함수 내에서 계산
-        # * 함수명 : std_weight
-        # * 전달값 : 키(height), 성별(gender)
+# * 함수명 : std_weight
+# * 전달값 : 키(height), 성별(gender)
 # 조건 2 : 표준 체중은 소수점 둘째자리까지 표시
 
 # (출력 예제)
@@ -18,9 +19,11 @@
 
 def std_weight(height, gender):
     if gender == "man":
-        print("키 {0}cm 남자의 표준 체중은 {1}kg 입니다.".format(height, (height*0.01)*(height*0.01)*22))
+        print("키 {0}cm 남자의 표준 체중은 {1}kg 입니다.".format(
+            height, (height*0.01)*(height*0.01)*22))
     elif gender == "woman":
-        print("키 {0}cm 여자의 표준 체중은 {1}kg 입니다.".format(height, (height*0.01)*(height*0.01)*21))
+        print("키 {0}cm 여자의 표준 체중은 {1}kg 입니다.".format(
+            height, (height*0.01)*(height*0.01)*21))
     else:
         print("입력이 올바르지 않습니다.")
 
