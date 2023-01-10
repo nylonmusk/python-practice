a148caf2ce4b724119fe24b5746ef4e1add9c797
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-01-10 14:51:37 +0900

    feat : module

---
+++ python-practice/module.py
@@ -0,0 +1,23 @@
+# module
+
+import theater_module
+theater_module.price(3) # 3명에서 영화 보러 갔을 때 가격
+theater_module.price_morning(4) # 4명에서 조조할인 영화 보러 갔을 때 가격
+theater_module.price_soldier(5) # 5명의 군인이 영화 보러 갔을 때 가격
+
+import theater_module as mv
+mv.price(3)
+mv.price_morning(4)
+mv.price_soldier(5)
+
+from theater_module import *
+price(3)
+price_morning(4)
+price_soldier(5)
+
+from theater_module import price, price_morning
+price(5)
+price_morning(6)
+
+from theater_module import price_soldier as pc
+pc(10)
